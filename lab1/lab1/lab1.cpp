21f27a958740993320f24a23e69be54e0137dc29
mio <opreamarianiulian@gmail.com>
2024-03-15 15:44:45 +0200

    class Carti

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -9,6 +9,13 @@ struct Persoana
     char nume[20];
     char prenume[20];
 };
+struct Carte
+{
+    char autor[20];
+    char editura[20];
+    unsigned anul_aparitie;
+    unsigned vechime;
+};
 
 class Pers
 {
@@ -28,9 +35,51 @@ public:
     char* arata_nume() { return this->nume; }
 };
 
+class Carti
+{
+private:
+    char autor[20];
+    char editura[20];
+    unsigned anul_aparitie;
+    unsigned vechime;
+public:
+    Carti(char* autor1, char* editura1, unsigned anul_aparitiei1, unsigned vechime1)
+    {
+        strcpy_s(autor, 20, autor1);
+        strcpy_s(editura, 20, editura1);
+        anul_aparitie = anul_aparitiei1;
+        vechime = vechime1;
+    }
+    Carti() {}
+    char* arata_autor() { return this->autor; }
+    unsigned arata_vechime() { return this->vechime; }
+};
+
 int main()
 {
-    Console::WriteLine("Introduceti numarul de persoane: ");
+    Console::WriteLine("Introduceti numarul de carti: ");
+    int nr_carti;
+    cin >> nr_carti;
+    Carte* carte = new Carte[nr_carti];
+    
+    for (int i = 0; i < nr_carti; i++)
+    {
+        Console::WriteLine("Introduceti numele autorului {0}: ", i + 1);
+        char autor[20];
+        cin >> autor;
+        strcpy_s(carte[i].autor, 20, autor);
+        Console::WriteLine("Introduceti vechimea cartii {0}: ", i + 1);
+        unsigned vechime;
+        cin >> vechime;
+        carte[i].vechime = vechime;
+    }
+    Console::WriteLine("Afisam numele autorului si vechimea cartii aferente: ");
+    for (int i = 0; i < nr_carti; i++)
+    {
+        cout << carte[i].autor << "\t" << carte[i].vechime << endl;
+    }
+    Console::ReadKey();
+    /*Console::WriteLine("Introduceti numarul de persoane: ");
     int nr_persoane;
     cin >> nr_persoane;
     Persoana* persoane = new Persoana[nr_persoane];
@@ -51,5 +100,6 @@ int main()
         cout << persoane[i].nume << "\t" << persoane[i].vs << endl;
     }
     Console::ReadKey();
+    */
     return 0;
 }
